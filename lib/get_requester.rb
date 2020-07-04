f071eb51b0945c477dcff6eab6696d8be94f1f42
Erin Mettille <ecmettille@gmail.com>
2020-07-04 17:55:44 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,17 +1,20 @@
-require 'net/http'
+
 require 'open-uri'
+require 'net/http'
 require 'json'
 
 class GetRequester
-  
-  def initalize(url)
-    @url = url 
+  def initialize(url)
+    @url = url
   end
-  
+
   def get_response_body
     uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
-    response.body  
+    response.body
   end
-  
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
 end
