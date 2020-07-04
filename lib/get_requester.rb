2b1b7fd55b54f31fab4b6204b17c0c0d95c48986
Erin Mettille <ecmettille@gmail.com>
2020-07-04 17:48:31 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,11 +5,11 @@ require 'json'
 class GetRequester
   
   def initalize(url)
-    @url = url 
+    URL = url 
   end
   
   def get_response_body
-    uri = URI.parse(@url)
+    uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body  
   end
